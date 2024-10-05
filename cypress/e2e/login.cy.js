bb41dd36987f956b66ef6ca39d40ada94c012e3f
Temitope Agbor <67988734+silverdruid@users.noreply.github.com>
2024-10-05 16:28:11 +0100

    updated commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,55 +3,47 @@ import  {login}  from './pages/loginPage.mjs'
 
 describe('login, create and delete course', () => {
   
-  // it('loads url successfully and confirms page information visibility', () => {
-  //   login.openApp()
-  //   login.confirmPageInformationVisibilty()
+  it('loads url successfully and confirms page information visibility', () => {
+    login.openApp()
+    login.confirmPageInformationVisibilty()
     
-  // })
+  })
    
-  // it('types corrrect email and Incorrect password', () => {
-  //    login.openApp()
-  //    login.confirmPageInformationVisibilty()
-  //    login.typeCorrectEmailAndIncorrectPwd()
-  //    login.clickLoginBtn()
-  //  })
+  it('types corrrect email and Incorrect password', () => {
+     login.openApp()
+     login.confirmPageInformationVisibilty()
+     login.typeCorrectEmailAndIncorrectPwd()
+     login.clickLoginBtn()
+   })
   
-  // it('types Incorrrect email and correct password', () => {
-  //    login.openApp()
-  //    login.confirmPageInformationVisibilty()
-  //    login.typeIncorrectEmailAndCorrectPwd()
-  //    login.clickLoginBtn()
-  // })
+  it('types Incorrrect email and correct password', () => {
+     login.openApp()
+     login.confirmPageInformationVisibilty()
+     login.typeIncorrectEmailAndCorrectPwd()
+     login.clickLoginBtn()
+  })
   
-  // it('types Incorrrect email and Incorrect password', () => {
-  //    login.openApp()
-  //    login.confirmPageInformationVisibilty()
-  //    login.typeIncorrectEmailAndIncorrectPwd()
-  //    login.clickLoginBtn()
-  // })
+  it('types Incorrrect email and Incorrect password', () => {
+     login.openApp()
+     login.confirmPageInformationVisibilty()
+     login.typeIncorrectEmailAndIncorrectPwd()
+     login.clickLoginBtn()
+  })
   
 
-  //   it('types correct email and correct password', () => {
-  //   login.openApp()
-  //   login.confirmPageInformationVisibilty()
-  //   login.typeCorrectEmailAndPwd()
-  //   login.clickLoginBtn()
-  // })
-  
-  // it('confirms course page visibilty', () => {
-  //   login.openApp()
-  //   login.confirmPageInformationVisibilty()
-  //   login.typeCorrectEmailAndPwd()
-  //   login.clickLoginBtn()
-  //   login.assertCoursePage()
-  // })
-
-  it('creates course', () => {
+    it('types correct email and correct password', () => {
     login.openApp()
     login.confirmPageInformationVisibilty()
     login.typeCorrectEmailAndPwd()
     login.clickLoginBtn()
-   login.createCourse()
+  })
+  
+  it('confirms course page visibilty', () => {
+    login.openApp()
+    login.confirmPageInformationVisibilty()
+    login.typeCorrectEmailAndPwd()
+    login.clickLoginBtn()
+    login.assertCoursePage()
   })
 
 
